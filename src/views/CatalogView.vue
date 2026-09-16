311e0531b4d398c974d4a884617b9a1535a7d66c
Adnan Zaki <adnzaki@outlook.com>
2026-09-16 16:49:12 +0700

    fix: booking text in e-book list

--- src/views/CatalogView.vue
+++ src/views/CatalogView.vue
@@ -301,7 +301,7 @@
                   : 'bg-slate-100 text-slate-400 cursor-not-allowed'"
               >
                 <Bookmark class="w-3 h-3 sm:w-3.5 sm:h-3.5" />
-                <span class="truncate">{{ book.availableCopies > 0 ? (book.isEbook ? 'Booking e-Book' : 'Hold 24h') : 'Habis' }}</span>
+                <span class="truncate">{{ book.availableCopies > 0 ? (book.isEbook ? 'Booking' : 'Hold 24h') : 'Habis' }}</span>
               </button>
             </div>
           </div>
